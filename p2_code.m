1f88ef2d46c0858d75ac10e31c13875d89b8f3e1
Iamiran1 <vinicius.c.miranda@icloud.com>
2025-05-13 17:24:19 -0300

    o juliao sabe

--- p2_code.m
+++ p2_code.m
@@ -1,29 +1,35 @@
+dados = readmatrix('entradas.xlsx');  % Lê sem se preocupar com nomes de colunas
 
+% Verifica se há pelo menos 3 colunas
+if size(dados, 2) >= 3
+    for i = 1:size(dados, 1)  % Percorre todas as linhas
+        % Verifica se algum valor da linha é nulo (NaN)
+        if any(isnan(dados(i, :)))
+            disp('Valor nulo encontrado, parando a execução.');
+            break;
+        end
+        
+        % Extrai os valores de a, b, c para a linha atual
+        a = dados(i, 1);
+        b = dados(i, 2);
+        c = dados(i, 3);
 
+        % Chamada da função
+        [s, p, m] = solucao_sistema_de_equacoes(a, b, c);
 
+        % Exibição dos resultados
+        fprintf('Linha %d:\n', i);
+        fprintf('Soma: %.2f\n', s);
+        fprintf('Produto: %.2f\n', p);
+        fprintf('Média: %.2f\n\n', m);
+    end
+else
+    disp('Erro: Arquivo não contém dados suficientes (pelo menos 1 linha e 3 colunas).');
+end
+
+% Função no final do arquivo
 function [soma, produto, media] = solucao_sistema_de_equacoes(a, b, c)
-    % operacoes_basicas - Realiza operações simples com três números
-    %
-    % Sintaxe:
-    %   [soma, produto, media] = operacoes_basicas(a, b, c)
-    %
-    % Entradas:
-    %   a, b, c - Números reais
-    %
-    % Saídas:
-    %   soma - Soma dos três números
-    %   produto - Produto dos três números
-    %   media - Média aritmética dos três números
-
     soma = a + b + c;
     produto = a * b * c;
     media = (a + b + c) / 3;
 end
-
-% Chamada da função
-[s, p, m] = solucao_sistema_de_equacoes(2, 4, 6);
-
-% Exibição dos resultados
-fprintf('Soma: %.2f\n', s);
-fprintf('Produto: %.2f\n', p);
-fprintf('Média: %.2f\n', m);
