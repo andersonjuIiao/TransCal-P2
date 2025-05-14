befb21ce9701096aa28dae384350f0c860687b8d
Iamiran1 <vinicius.c.miranda@icloud.com>
2025-05-14 17:24:44 -0300

    adicionei os nós e coordenadas a tabela

--- p2_code.m
+++ p2_code.m
@@ -15,16 +15,25 @@ function tabela = propriedades_elementos(dados)
 
     E = 210e9;   % Pa // DEVE SER PASSADO MAS É PROVISÓRIO
     A = 2e-4;    % m² // DEVE SER PASSADO MAS É PROVISÓRIO
+    nos_list = zeros(n_elem, 1);
+    x_list = zeros(n_elem, 1);
+    y_list = zeros(n_elem, 1);
 
     c_list = zeros(n_elem, 1);
     s_list = zeros(n_elem, 1);
     L_list = zeros(n_elem, 1);
+    
     dofs = zeros(n_elem, 4);
 
     for i = 1:n_elem
+
+        nos_list(i) =  dados(i, 1);
+        x_list(i) = dados(i, 2);
+        y_list(i) = dados(i, 3);
+        
         no_i = i;
         no_j = mod(i, n_nos) + 1;  % Nó seguinte (circular)
-
+        
         xi = dados(no_i, 2);  yi = dados(no_i, 3);
         xj = dados(no_j, 2);  yj = dados(no_j, 3);
 
@@ -37,14 +46,18 @@ function tabela = propriedades_elementos(dados)
         s_list(i) = s;
 
         dofs(i, :) = [2*no_i - 1, 2*no_i, 2*no_j - 1, 2*no_j];
+
     end
 
-    tabela = table((1:n_elem)', ...
+    tabela = table(nos_list, ...
+                   x_list, ...
+                   y_list, ...
+                   (1:n_elem)', ...
                    repmat(A, n_elem, 1), ...
                    repmat(E, n_elem, 1), ...
                    c_list, ...
                    s_list, ...
                    L_list, ...
                    dofs, ...
-                   'VariableNames', {'Elemento', 'Area_m2', 'E_Pa', 'c', 's', 'L_m', 'Graus_de_Liberdade'});
+                   'VariableNames', {'Número do nó','x (m)','y (m)' ,'Elemento', 'Area (m^2)', 'E (Pa)', 'c', 's', 'L (m)', 'Graus_de_Liberdade'});
 end
