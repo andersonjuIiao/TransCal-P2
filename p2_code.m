75d71ff7efa168547af30fb0f25a77a22a0752eb
Iamiran1 <vinicius.c.miranda@icloud.com>
2025-05-15 07:45:14 -0300

    adicionando matriz  de rigidez

--- p2_code.m
+++ p2_code.m
@@ -1,20 +1,16 @@
 % Lê os dados do Excel (sem cabeçalhos)
 dados = readmatrix('entradas.xlsx');
-dados = dados(:, 1:3);  % Mantém apenas as colunas: Nó, X, Y
+dados = dados(:, 1:5);  % Mantém apenas as colunas: Nó, X, Y,E,A
 
-% Chama a função
-tabela = propriedades_elementos(dados);
-
-% Exibe a tabela
-disp(tabela)
 
 % Função sem conectividade explícita
 function tabela = propriedades_elementos(dados)
     n_nos = size(dados, 1);
     n_elem = n_nos;
 
-    E = 210e9;   % Pa // DEVE SER PASSADO MAS É PROVISÓRIO
-    A = 2e-4;    % m² // DEVE SER PASSADO MAS É PROVISÓRIO
+
+    E_list = zeros(n_elem, 1);
+    A_list = zeros(n_elem, 1);
     nos_list = zeros(n_elem, 1);
     x_list = zeros(n_elem, 1);
     y_list = zeros(n_elem, 1);
@@ -30,6 +26,9 @@ function tabela = propriedades_elementos(dados)
         nos_list(i) =  dados(i, 1);
         x_list(i) = dados(i, 2);
         y_list(i) = dados(i, 3);
+        E_list(i)=  dados(i, 4);
+        A_list(i)=  dados(i, 5);
+
         
         no_i = i;
         no_j = mod(i, n_nos) + 1;  % Nó seguinte (circular)
@@ -48,16 +47,56 @@ function tabela = propriedades_elementos(dados)
         dofs(i, :) = [2*no_i - 1, 2*no_i, 2*no_j - 1, 2*no_j];
 
     end
-
+         
     tabela = table(nos_list, ...
                    x_list, ...
                    y_list, ...
                    (1:n_elem)', ...
-                   repmat(A, n_elem, 1), ...
-                   repmat(E, n_elem, 1), ...
+                   A_list, ...
+                   E_list, ...
                    c_list, ...
                    s_list, ...
                    L_list, ...
                    dofs, ...
                    'VariableNames', {'Número do nó','x (m)','y (m)' ,'Elemento', 'Area (m^2)', 'E (Pa)', 'c', 's', 'L (m)', 'Graus_de_Liberdade'});
 end
+
+
+function lista_matrizes_rigidez = matriz_rigized(tabela)
+    Ke_cel = cell(height(tabela), 1);
+    nomes = strings(height(tabela), 1);  % vetor de nomes "Ke1", "Ke2", ...
+
+    for  i = 1:height(tabela)
+            E = tabela.('E (Pa)')(i);
+            A = tabela.('Area (m^2)')(i);
+            l = tabela.('L (m)')(i);
+            c = tabela.('c')(i);
+            s = tabela.('s')(i);
+            M = [c^2,c*s,-c^2,-c*s;
+                c*s, s^2, -c*s, -s^2;
+                -c^2,-c*s, c^2 , c*s;
+                -c*s, -s^2, c*s, s^2];
+            
+            Ke = ((E*A)/l) * M;
+            Ke_cel{i} = Ke;
+            nomes(i) = "Ke" + string(i);  % Cria nome: "Ke1", "Ke2", ...
+    end
+    lista_matrizes_rigidez = table(nomes, Ke_cel, ...
+        'VariableNames', {'Nome', 'Matriz_Ke'});
+
+
+end
+
+
+
+% Chama a função
+tabela = propriedades_elementos(dados);
+matrizes_rigidez = matriz_rigized(tabela);
+% Exibe a tabela
+disp(tabela)
+disp(matrizes_rigidez)
+% Exibir nome da 2ª matriz
+% disp(matrizes_rigidez.Nome(2))  % --> "Ke2"
+
+% Exibir a matriz correspondente
+% disp(matrizes_rigidez.Matriz_Ke{2})
